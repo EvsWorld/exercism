7d9df0a017292a93a7a5103c979e3bf9bb55f20e
evsworld <evanlhendrix@gmail.com>
2022-02-11 19:18:30 +0100

    first pass resistor trio

---
+++ resistor-color-trio/resistor-color-trio.ts
@@ -0,0 +1,32 @@
+export function decodedResistorValue(colors: string[]) {
+  // throw new Error('Remove this statement and implement this function')
+
+  const dict: { [key: string]: number } = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9,
+  };
+  const firstColor: string = colors[0];
+  const secondColor: string = colors[1];
+  const thirdColor: string = colors[2];
+  console.log(`firstVal: ${dict[firstColor]}`);
+  console.log(`secondVal: ${dict[secondColor]}`);
+  const numberOfZeros = dict[thirdColor];
+  console.log("numberOfZeros :>> ", numberOfZeros);
+  const expected = Number(`${dict[firstColor]}${dict[secondColor]}`);
+  let zerosString = "";
+  for (let i = 0; i < numberOfZeros; i++) {
+    zerosString += 0;
+  }
+  console.log("zerosString :>> ", zerosString);
+  const wZeros = `${expected}${zerosString}`;
+  console.log("wZeros :>> ", wZeros);
+  return wZeros + " ohms";
+}

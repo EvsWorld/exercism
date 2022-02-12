16106be137509375677752701e6a2cd0ef2ee496
evsworld <evanlhendrix@gmail.com>
2022-02-12 14:02:09 +0100

    wip: still not working

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,19 +1,26 @@
 export function isLeap(year: number) {
-  if (year % 4 === 0) {
+  const divisibleBy400 = year % 400 === 0;
+  const divisibleBy4 = year % 4 === 0;
+  const divisibleBy100 = year % 100 === 0;
+  if (divisibleBy4) {
     console.log("divisible by 4");
-    if (year % 100 !== 0) {
-      console.log("divisible by 4 but not 100");
+    if (divisibleBy100) {
+      console.log("divisible by 4 and 100, so not leap year");
       // unless... if its divisible by 400, then is leap year
-      if (year % 400 === 0) {
-        console.log(" divisible by 4 but not 100 but is divisible by 400 ");
+      if (divisibleBy400) {
+        console.log(
+          "divisible by 4 and 100(not lp yr) but is divisible by 400, (so is lp yr)"
+        );
         return true;
       }
-      console.log(" divisible by 4 but not 100 and not divisible by 400 ");
+      console.log(
+        " divisible by 4 and 100 (not lp yr) but not divisible by 400 "
+      );
       return false;
     }
-    console.log("divisible by 4 and 100");
+    console.log("divisible by 4 but not 100");
     return true;
   }
   console.log("not divisible by 4");
-  return false;
+  return true;
 }

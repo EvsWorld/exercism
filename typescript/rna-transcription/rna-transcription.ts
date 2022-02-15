0a074c2b45c65cd1a872422685ff72c6238ff08b
evsworld <evanlhendrix@gmail.com>
2022-02-15 21:12:56 +0100

    refactor: rna w map method

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,17 +1,18 @@
+type Nucleotide = "G" | "C" | "A" | "T";
 export function toRna(dna: string) {
-  const trans: { [key]: string } = {
+  const trans: { [Nucleotide: string]: string } = {
     G: "C",
     C: "G",
     T: "A",
     A: "U",
   };
-  const resArr: string[] = [];
-  dna.split("").forEach((d) => {
-    if (!["G", "C", "T", "A"].includes(d)) {
-      throw new Error("Invalid input DNA.");
-    }
-    resArr.push(trans[d]);
-  });
-  return resArr.join("");
-  // if (dna === "C") return "G";
+  return dna
+    .split("")
+    .map((d) => {
+      if (!["G", "C", "T", "A"].includes(d)) {
+        throw new Error("Invalid input DNA.");
+      }
+      return trans[d];
+    })
+    .join("");
 }

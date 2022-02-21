828642ee7168d468ba333c0bb9b02ad024e4d1e2
evsworld <evanlhendrix@gmail.com>
2022-02-21 20:56:30 +0100

    refactor: dnd with constructor

--- typescript/dnd-character/dnd-character.ts
+++ typescript/dnd-character/dnd-character.ts
@@ -3,7 +3,7 @@ function removeSmallest(arr: number[]) {
   return arr.filter((e) => e !== min);
 }
 
-interface IDnDCharacter {
+export class DnDCharacter {
   constitution: number;
   strength: number;
   dexterity: number;
@@ -11,15 +11,16 @@ interface IDnDCharacter {
   intelligence: number;
   charisma: number;
   hitpoints: number;
-}
-export class DnDCharacter implements IDnDCharacter {
-  constitution = DnDCharacter.generateAbilityScore();
-  strength = DnDCharacter.generateAbilityScore();
-  dexterity = DnDCharacter.generateAbilityScore();
-  wisdom = DnDCharacter.generateAbilityScore();
-  intelligence = DnDCharacter.generateAbilityScore();
-  charisma = DnDCharacter.generateAbilityScore();
-  hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution);
+
+  constructor() {
+    this.constitution = DnDCharacter.generateAbilityScore();
+    this.strength = DnDCharacter.generateAbilityScore();
+    this.dexterity = DnDCharacter.generateAbilityScore();
+    this.wisdom = DnDCharacter.generateAbilityScore();
+    this.intelligence = DnDCharacter.generateAbilityScore();
+    this.charisma = DnDCharacter.generateAbilityScore();
+    this.hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution);
+  }
 
   public static generateAbilityScore(): number {
     let rolls = [...Array(4)].map((d) => this.generateRandomDiceRollScore());
@@ -34,9 +35,14 @@ export class DnDCharacter implements IDnDCharacter {
   public static getModifierFor(abilityValue: number): number {
     return Math.floor((abilityValue - 10) / 2);
   }
+
   private static generateRandomDiceRollScore(): number {
     return Math.round(Math.random() * 5 + 1);
   }
 }
+
 console.log(DnDCharacter.getModifierFor(3));
 console.log(DnDCharacter.generateAbilityScore());
+console.log(DnDCharacter.generateRandomDiceRollScore());
+const mydnd = new DnDCharacter();
+console.log("mydnd.strength :>> ", mydnd.strength);

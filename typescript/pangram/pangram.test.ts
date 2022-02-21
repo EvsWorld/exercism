79bea6e73bbeb8d1606e8c0217f3d65b7661cc68
evsworld <evanlhendrix@gmail.com>
2022-02-21 21:37:44 +0100

    imp: first working solution

--- typescript/pangram/pangram.test.ts
+++ typescript/pangram/pangram.test.ts
@@ -33,17 +33,17 @@ describe("Pangram()", () => {
     );
   });
 
-  xit("missing letters replaced by numbers", () => {
+  it("missing letters replaced by numbers", () => {
     expect(isPangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog")).toBe(
       false
     );
   });
 
-  xit("mixed case and punctuation", () => {
+  it("mixed case and punctuation", () => {
     expect(isPangram('"Five quacking Zephyrs jolt my wax bed."')).toBe(true);
   });
 
-  xit("case insensitive", () => {
+  it("case insensitive", () => {
     expect(isPangram("the quick brown fox jumps over with lazy FX")).toBe(
       false
     );
--- typescript/pangram/pangram.ts
+++ typescript/pangram/pangram.ts
@@ -4,11 +4,12 @@ export function isPangram(s: string) {
   // iterate over and check if exists in alphabet table?  iterate over and mark
   // if its unique so far, then if so, increment counter, then at the end if the
   // counter == 27 then its a panagram
-  const unique = new Set([...s.split("")]);
+  const stringArray = s.split("").map((char) => char.toLowerCase());
+  const unique = new Set([...stringArray]);
   let uniqueArray = Array.from(unique);
   uniqueArray = uniqueArray.filter((char, index, arr) => {
-    // console.log("char :>> ", char);
-    return !Number.isInteger(char) && char !== "_" && char !== " ";
+    console.log("!Number.isInteger(char)  :>> ", !Number.isInteger(char));
+    return !Number.isInteger(+char) && !["_", " ", ".", '"'].includes(char);
   });
   console.log("\n\n string = : ", s);
   console.log("uniqueArray: " + uniqueArray + "\n");

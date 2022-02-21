b6164c839ff1833925ce5cfe3d2b9b48122cb167
evsworld <evanlhendrix@gmail.com>
2022-02-21 17:23:49 +0100

    refactor: space age types

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -9,20 +9,36 @@ function roundTo(n: number, digits: number) {
   return +test.toFixed(digits);
 }
 
-export function age(planet: string, seconds: number): number {
+type Planets =
+  | "mercury"
+  | "venus"
+  | "earth"
+  | "mars"
+  | "jupiter"
+  | "saturn"
+  | "uranus"
+  | "neptune";
+
+const orbitRatio: { [key in Planets]: number } = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
+
+type Planet = keyof typeof orbitRatio;
+
+export function age(planet: Planet, seconds: number): number {
   // throw new Error('Remove this statement and implement this function')
   const earthSeconds = 31557600;
   const earthDays = 365.25;
 
-  const planets: { [key: string]: number } = {
-    mercury: 0.2408467,
-    venus: 0.61519726,
-    earth: 1,
-    mars: 1.8808158,
-    jupiter: 11.862615,
-    saturn: 29.447498,
-    uranus: 84.016846,
-    neptune: 164.79132,
-  };
-  return roundTo(seconds / (planets[planet] * earthSeconds), 2);
+  return roundTo(seconds / (orbitRatio[planet] * earthSeconds), 2);
 }
+
+// can't input fake
+// age("fake", 23);

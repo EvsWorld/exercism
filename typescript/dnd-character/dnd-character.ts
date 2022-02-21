14ae899e87c1c1a948c556a1c8e2003778646e23
evsworld <evanlhendrix@gmail.com>
2022-02-21 20:46:47 +0100

    refactor: dnd with static private method

--- typescript/dnd-character/dnd-character.ts
+++ typescript/dnd-character/dnd-character.ts
@@ -22,20 +22,20 @@ export class DnDCharacter implements IDnDCharacter {
   hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution);
 
   public static generateAbilityScore(): number {
-    const roll = () => Math.round(Math.random() * 5 + 1);
-    let tries = [0, 0, 0, 0].map((d) => {
-      return roll();
-    });
-    console.log("tries :>> ", tries);
-    tries = removeSmallest(tries);
-    console.log("tries :>> ", tries);
-    const sum = tries.reduce((acc, current) => acc + current);
+    let rolls = [...Array(4)].map((d) => this.generateRandomDiceRollScore());
+    console.log("rolls :>> ", rolls);
+    rolls = removeSmallest(rolls);
+    console.log("rolls :>> ", rolls);
+    const sum = rolls.reduce((acc, current) => acc + current);
     console.log("sum :>> ", sum);
     return sum;
   }
 
   public static getModifierFor(abilityValue: number): number {
     return Math.floor((abilityValue - 10) / 2);
+  }
+  private static generateRandomDiceRollScore(): number {
+    return Math.round(Math.random() * 5 + 1);
   }
 }
 console.log(DnDCharacter.getModifierFor(3));

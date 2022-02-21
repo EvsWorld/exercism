b3d3492fb91518b8b05a9496936b3734c5630a01
evsworld <evanlhendrix@gmail.com>
2022-02-21 17:00:42 +0100

    imp: space age

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,3 +1,21 @@
-export function age(planet: unknown, seconds: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+function roundToTwo(num) {
+  return +(Math.round(num + "e+2") + "e-2");
+}
+
+export function age(planet: string, seconds: number): number {
+  // throw new Error('Remove this statement and implement this function')
+  const earthSeconds = 31557600;
+  const earthDays = 365.25;
+
+  const planets: { [key: string]: number } = {
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+  };
+  return roundToTwo(seconds / (planets[planet] * earthSeconds));
 }

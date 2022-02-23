9259d4de3b37304753087cc598e85f36bc7aeb68
evsworld <evanlhendrix@gmail.com>
2022-02-23 18:54:26 +0100

    imp: bob: solve silence cases

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,5 +1,7 @@
 const isSilence = (m: string) => {
-  const r = /[ \t\n\r\s]+/.test(m) || m === "";
+  // const r = /[\t\n\r]+/.test(m) || m === "";
+  const r = /^\s*$/.test(m) || m === "";
+  console.log("isSilence = ", r);
   return r;
 };
 const isQuestionMarkOnEnd = (m: string) => {
@@ -9,6 +11,7 @@ const isQuestionMarkOnEnd = (m: string) => {
 };
 const isTherePunctuation = (m: string) => {
   const r = ["!", "?", "."].includes(m.slice(-1));
+  // console.log("isTherePunctuation :>> ", r);
   return r;
 };
 
@@ -19,12 +22,12 @@ const isAllCaps = (m: string) => {
   console.log("firstChars :>> ", firstChars(m));
   const firstCharsArray = Array.from(firstChars(m));
   const allUppercaseLetters = firstCharsArray.every((char) => {
-    console.log("char :>> ", char);
-    console.log(
-      "char.toUpperCase() === char :>> ",
-      char.toUpperCase() === char
-    );
-    return char.toUpperCase() === char || [" ", ",", "."].includes(char);
+    // console.log("char :>> ", char);
+    // console.log(
+    //   "char.toUpperCase() === char :>> ",
+    //   char.toUpperCase() === char
+    // );
+    return char.toUpperCase() === char;
   });
   const allNumbers = firstCharsArray.every((char) => {
     return !isNaN(+char) || [" ", ",", "."].includes(char);
@@ -52,9 +55,9 @@ export function hey(message: string): string {
       console.log("YELL");
       response = "Whoa, chill out!";
       break;
-    case isSilence(message) &&
-      !isAllCaps(message) &&
-      !isQuestionMarkOnEnd(message):
+    case isSilence(message):
+      // !isAllCaps(message) &&
+      // !isQuestionMarkOnEnd(message):
       console.log("silence");
       response = "Fine. Be that way!";
       break;

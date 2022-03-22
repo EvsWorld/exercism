ecf59a177c8b62b2de88e5b0c4ad86d28f579c66
evsworld <evanlhendrix@gmail.com>
2022-03-22 01:14:56 +0100

    imp: first correct solution for matrix

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -1,19 +1,62 @@
 export class Matrix {
-  _rows = 0;
-  _colums = m;
-  constructor(m: Matrix) {
-    // throw new Error('Remove this statement and implement this function')
+  constructor(public m: string) {}
+
+  private transposeArray(
+    array: number[][],
+    arrayLength: number = array.length
+  ) {
+    var newArray: number[][] = [];
+    for (var i = 0; i < array.length; i++) {
+      newArray.push([]);
+    }
+
+    for (var i = 0; i < array.length; i++) {
+      for (var j = 0; j < arrayLength; j++) {
+        newArray[j].push(array[i][j]);
+      }
+    }
+
+    return newArray;
   }
 
-  get rows(): number[] {
-    // throw new Error('Remove this statement and implement this function')
-    return m["_rows"];
+  private rowsOfIntFromString(s: string) {
+    const rowsArray = s.split("\n");
+    const rowsArrayNum = rowsArray.map((e: string) => {
+      return e.split(" ").map((n: string) => Number(n));
+    });
+    return rowsArrayNum;
   }
 
-  get columns(): number[] {
-    // throw new Error('Remove this statement and implement this function')
-    return [this._colums];
+  private transpose(matrix: number[][]): number[][] {
+    return matrix[0].map((_, colIndex) => matrix.map((row) => row[colIndex]));
+  }
+
+  get rows(): number[][] {
+    return this.rowsOfIntFromString(this.m);
+  }
+
+  // works
+  get columns1(): number[][] {
+    const final: number[][] = [];
+    for (let outerIn = 0; outerIn < this.rows[0].length; outerIn++) {
+      const column = this.rows.map((_, innerIn: number) => {
+        return this.rows[innerIn][outerIn];
+      });
+      final.push(column);
+    }
+    return final;
+  }
+
+  // works
+  get columns(): number[][] {
+    return this.transpose(this.rows);
+  }
+
+  // doesnt work
+  get columns3(): number[][] {
+    return this.transposeArray(this.rows);
   }
 }
 
-console.log(new Matrix("1 2\n3 4"));
+console.log(new Matrix("1 2 3\n4 5 6").rows);
+console.log(new Matrix("1 2 3\n4 5 6").columns);

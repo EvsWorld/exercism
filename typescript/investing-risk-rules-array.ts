190bb19a067295df73ebca676857508041d00b59
evsworld <evanlhendrix@gmail.com>
2022-04-06 18:02:42 +0200

    reorganize

--- typescript/investing-risk-rules-array.ts
+++ typescript/investing-risk-rules-array.ts
@@ -2,6 +2,25 @@
 // each contribution has entity object associated
 // the percentages dont add up to 100
 
+// ************************** Types And Data *****************************
+// unused
+const rulesArray: Rule[] = [
+  {
+    minThreshold: 46,
+    minScore: 56,
+    maxThreshold: 84,
+    maxScore: 1,
+    entities: ["Agora", "Nucleus Market", "Entity2"],
+  },
+  {
+    minThreshold: 69,
+    minScore: 2,
+    maxThreshold: 88,
+    maxScore: 13,
+    categories: ["Dark Market", "Another Category", "Some Category"],
+  },
+];
+
 type Rule = {
   minThreshold: number;
   minScore: number;
@@ -29,114 +48,114 @@ const categoriesAndEntities: { [category: string]: string[] } = {
   "Category B": ["Entity2", "Entity3"],
 };
 
-// ! I think they might have the structure a bit different. It might be a rules dictionary or array
-// yea, I think they want me to loop over this and then decide to apply each rule or not.
+// ******************************** Functions *****************************
+
+// Calculate rules by linear percentage contribution, and done as follows: If
+// 1. the contribution of funds is less than the min threshold, then the rule
+//    is not triggered;
+// 2. If the contribution of funds equals the min threshold, then the min
+//    score is applied;
+// 3.  If the contribution of funds is equal to or greater than the max
+//     threshold, then the max score is applied;
 //
-const rulesArray: Rule[] = [
-  {
-    minThreshold: 46,
-    minScore: 56,
-    maxThreshold: 84,
-    maxScore: 1,
-    entities: ["Agora", "Nucleus Market", "Entity2"],
-  },
-  {
-    minThreshold: 69,
-    minScore: 2,
-    maxThreshold: 88,
-    maxScore: 13,
-    categories: ["Dark Market", "Another Category", "Some Category"],
-  },
-];
+// So i need a function to apply these rules once I get the rules that do
+// apply
+//
+// Example of Linear percentage contribution:
+// For any percentage in between, the score assigned is on a linear scale
+//  between the given min and max scores. For example, for a rule like:
 
-export const investingRisk = (transactionAnalysis: TransactionAnalysis) => {
-  // Calculate rules by linear percentage contribution, and done as follows: If
-  // 1. the contribution of funds is less than the min threshold, then the rule
-  //    is not triggered;
-  // 2. If the contribution of funds equals the min threshold, then the min
-  //    score is applied;
-  // 3.  If the contribution of funds is equal to or greater than the max
-  //     threshold, then the max score is applied;
-  //
-  // So i need a function to apply these rules once I get the rules that do
-  // apply
-  //
-  // Example of Linear percentage contribution:
-  // For any percentage in between, the score assigned is on a linear scale
-  //  between the given min and max scores. For example, for a rule like:
+// * Min threshold: 20, Min score: 1
+// * Max threshold: 80, Max score: 7
 
-  // * Min threshold: 20, Min score: 1
-  // * Max threshold: 80, Max score: 7
+// And this rule is triggered with a contribution of 50%, a score of 4 is assigned. All
+// scores are rounded up to the nearest integer.
+// score = minScore + ( (contribution - minThreshold ) * ( maxScore - minScore ) / (maxThreshold - minThreshold) )
+// score =    1     + ( (     50      -     20       ) * (   7      -    1     ) / (     80      -     20      ) )
 
-  // And this rule is triggered with a contribution of 50%, a score of 4 is assigned. All
-  // scores are rounded up to the nearest integer.
-  // score = minScore + ( (contribution - minThreshold ) * ( maxScore - minScore ) / (maxThreshold - minThreshold) )
-  // score =    1     + ( (     50      -     20       ) * (   7      -    1     ) / (     80      -     20      ) )
-
-  const getScoreFromRule = (rule: Rule, contribution: number): number => {
-    let score: number = 0;
-    const { minThreshold, minScore, maxThreshold, maxScore } = rule;
-    if (contribution > minThreshold) {
-      // trigger rule
-      if (contribution === minThreshold) {
-        // apply min score
-        score = minScore;
-      } else if (contribution >= maxThreshold) {
-        // apply max score
-        score = maxScore;
-      } else {
-        // assign score based on linear scale
-        score =
-          minScore +
-          ((contribution - minThreshold) * (maxScore - minScore)) /
-            (maxThreshold - minThreshold);
-      }
+const getScoreFromRule = (rule: Rule, contribution: number): number => {
+  let score: number = 0;
+  const { minThreshold, minScore, maxThreshold, maxScore } = rule;
+  if (contribution > minThreshold) {
+    // trigger rule
+    if (contribution === minThreshold) {
+      // apply min score
+      score = minScore;
+    } else if (contribution >= maxThreshold) {
+      // apply max score
+      score = maxScore;
     } else {
-      // dont apply rule
+      // assign score based on linear scale
+      score =
+        minScore +
+        ((contribution - minThreshold) * (maxScore - minScore)) /
+          (maxThreshold - minThreshold);
     }
-    // do what with the score for this rule here? Add it to what?
-    return score;
-  };
+  } else {
+    // dont apply rule
+  }
+  // do what with the score for this rule here? Add it to what?
+  return score;
+};
 
-  const getCategory = (entity: Entity) => {
-    const results: string[] = [];
-    for (const category in categoriesAndEntities) {
-      const entitiesArray = categoriesAndEntities[category];
-      // console.log(`${category}: ${categoriesAndEntities[category]}`);
-      if (entitiesArray.includes(entity.name)) {
-        results.push(category);
-      }
+const getCategory = (entity: Entity) => {
+  const results: string[] = [];
+  for (const category in categoriesAndEntities) {
+    const entitiesArray = categoriesAndEntities[category];
+    // console.log(`${category}: ${categoriesAndEntities[category]}`);
+    if (entitiesArray.includes(entity.name)) {
+      results.push(category);
     }
-    return results[0];
-  };
+  }
+  return results[0];
+};
 
-  // ? what to do if there are overlapping rules? which should supercede? Probably the market
-  const getRulesForEntity = (entity: Entity) => {
-    // TODO: merge rules from category with rules from entity. Market superceeds.
-    const categories = getCategory(entity);
-    const category = categories[0];
-    // const categoryRule = getCategoryRule(categories[0]);
-    // iterate over rulesArray to get categoryRule or entityRule
-    const categoryRule = rulesByCategory[category];
-    const entityRule = rulesByEntity[entity.name];
-    console.log("categoryRule :>> ", categoryRule);
-    console.log("entityRule :>> ", entityRule);
-    const mergedRules = { ...entityRule, ...categoryRule };
-    console.log("mergedRules :>> ", mergedRules);
-    return mergedRules;
-  };
-
-  // TODO: reduce transactionAnalysis to include the category too
-  const completeTransactionAnalysis = transactionAnalysis.reduce((acc, cur) => {
+// TODO: reduce transactionAnalysis to include the category too
+const getCompleteTransactionAnalysis = (transactionAnalysisLocal) => {
+  return transactionAnalysisLocal.reduce((acc, cur) => {
     const category = getCategory(cur);
     const obj = { ...cur, category };
     acc.push(obj);
     return acc;
   }, []);
-  console.log("completeTransactionAnalysis :>> ", completeTransactionAnalysis);
+};
 
+const getRulesForEntity = (entity: Entity) => {
+  // TODO: merge rules from category with rules from entity. Market superceeds.
+  const categories = getCategory(entity);
+  const category = categories[0];
+  // const categoryRule = getCategoryRule(categories[0]);
+  // iterate over rulesArray to get categoryRule or entityRule
+  const categoryRule = rulesByCategory[category];
+  const entityRule = rulesByEntity[entity.name];
+  console.log("categoryRule :>> ", categoryRule);
+  console.log("entityRule :>> ", entityRule);
+  const mergedRules = { ...entityRule, ...categoryRule };
+  console.log("mergedRules :>> ", mergedRules);
+  return mergedRules;
+};
+
+//
+// Each rule is evaluated only if funds have been contributed from matching
+// categories/entities business logic for multiple matching rules is
+// *unspecified*
+//
+// ASSUMPTION:  I think this means that i only need to find one rule set to
+// apply to a given transaction
+//
+// TODO: We are evaluating a transaction analysis. This is a list of entities and their contributions. Each entity will get a score calculated for it.
+export const investingRisk = (transactionAnalysis: TransactionAnalysis) => {
+  // ? what to do if there are overlapping rules? which should supercede? Probably the market
+
+  const completeTransactionAnalysis =
+    getCompleteTransactionAnalysis(transactionAnalysis);
+  console.log("completeTransactionAnalysis :>> ", completeTransactionAnalysis);
+};
+console.log(investingRisk(transactionAnalysis));
+
+const calcScoreByIteratingOverRulesArray = (rulesArrayLocal) => {
   let scoreCount: number = 0;
-  rulesArray.forEach((rule) => {
+  rulesArrayLocal.forEach((rule) => {
     console.log("rule :>> ", rule);
     let contribution: number | null = null;
     // get entity if the rule matched by entity
@@ -171,15 +190,4 @@ export const investingRisk = (transactionAnalysis: TransactionAnalysis) => {
     // here choose whether to use the calculate score based on rule for
     // assign to scoreCount somehow
   });
-
-  //
-  // Each rule is evaluated only if funds have been contributed from matching
-  // categories/entities business logic for multiple matching rules is
-  // *unspecified*
-  //
-  // ASSUMPTION:  I think this means that i only need to find one rule set to
-  // apply to a given transaction
-  //
-  // TODO: We are evaluating a transaction analysis. This is a list of entities and their contributions. Each entity will get a score calculated for it.
 };
-console.log(investingRisk(transactionAnalysis));

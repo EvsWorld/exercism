ff8cead0ffe12e3d4d66d2e19725e12577cc587f
evsworld <evanlhendrix@gmail.com>
2022-04-07 11:45:15 +0200

    refactor: make functions more inempotent, and add types

--- typescript/interview-gather/investing-risk.test.ts
+++ typescript/interview-gather/investing-risk.test.ts
@@ -12,35 +12,35 @@ type Rule = {
 
 type Source = { name: string; contribution: number; category: string };
 
-const transactionAnalysisData: Source[] = [
-  { name: "Agora", category: "Dark Market", contribution: 45 },
-  { name: "Nucleus Market", category: "Dark Market", contribution: 76 },
-];
+describe("Calculates score for transaction analysis and rule set", () => {
+  it("Rule set has one rule with categories array and another rule with entities array and categories are the same for both sources", () => {
+    const transactionAnalysisData: Source[] = [
+      { name: "Agora", category: "Dark Market", contribution: 45 },
+      { name: "Nucleus Market", category: "Dark Market", contribution: 76 },
+    ];
 
-const rulesArrayData: Rule[] = [
-  {
-    minThreshold: 46,
-    minScore: 56,
-    maxThreshold: 84,
-    maxScore: 1,
-    entities: ["Agora", "Nucleus Market", "Entity2"],
-  },
-  {
-    minThreshold: 69,
-    minScore: 2,
-    maxThreshold: 88,
-    maxScore: 13,
-    categories: ["Dark Market", "Another Category", "Some Category"],
-  },
-  {
-    minThreshold: 37,
-    minScore: 4,
-    maxThreshold: 58,
-    maxScore: 19,
-  },
-];
-describe("hasIdenticalSetOfUniqueCharactersOnePass", () => {
-  it("Returns true for unique characters", () => {
+    const rulesArrayData: Rule[] = [
+      {
+        minThreshold: 46,
+        minScore: 56,
+        maxThreshold: 84,
+        maxScore: 1,
+        entities: ["Agora", "Nucleus Market", "Entity2"],
+      },
+      {
+        minThreshold: 69,
+        minScore: 2,
+        maxThreshold: 88,
+        maxScore: 13,
+        categories: ["Dark Market", "Another Category", "Some Category"],
+      },
+      {
+        minThreshold: 37,
+        minScore: 4,
+        maxThreshold: 58,
+        maxScore: 19,
+      },
+    ];
     expect(investingRisk(transactionAnalysisData, rulesArrayData)).toEqual(6);
   });
 });
